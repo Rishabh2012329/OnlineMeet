9bce68bcdccec74defe9d27105bea533057d5472
Rishabh <baidrishabh443@gmail.com>
2022-12-21 21:04:34 +0530

    added cors

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const cookieParser = require('cookie-parser')
 const roomRoute = require('./Routes/room')
 const path = require('path')
 var ObjectId = require('mongoose').Types.ObjectId;
+const cors = require('cors')
 
 dotenv.config()
 
@@ -28,12 +29,11 @@ mongoose.connect(process.env.MONGO_URL||"",(error,res)=>{
 
 app.set('view engine','ejs')
 app.set('views', path.join(__dirname, 'views'));
+app.use(cors())
 app.use(express.static('public'))
 app.use(express.json())
 app.use(cookieParser())
 app.use('/peerjs',peerServrer)
-
-
 app.use('/user',userRoute)
 app.use('/room',roomRoute)
 
